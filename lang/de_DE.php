26a9e889b256118f8313f5d5dd2c89a99f51d121
Christian Bachhelm <bachhelmc@p7media.de>
2011-11-27 12:35:21 +0100

    ENHANCEMENT enabled translations

---
+++ lang/de_DE.php
@@ -0,0 +1,12 @@
+<?php
+
+global $lang;
+
+$lang['de_DE']['GalleryImage']['CAPTION'] = 'Bildlegende';
+$lang['de_DE']['GalleryImage']['IMAGE'] = 'Bild';
+$lang['de_DE']['GalleryImage']['NOIMAGE'] = '(Kein Bild)';
+
+$lang['de_DE']['GalleryPage']['CAPTION'] = 'Bildlegende';
+$lang['de_DE']['GalleryPage']['THUMBNAIL'] = 'Miniaturbild';
+$lang['de_DE']['GalleryPage']['TITLE'] = 'Galerie Bild';
+$lang['de_DE']['GalleryPage']['HEADING'] = 'Galerie';
---
+++ lang/en_US.php
@@ -0,0 +1,12 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['GalleryImage']['CAPTION'] = 'Caption';
+$lang['en_US']['GalleryImage']['IMAGE'] = 'Image';
+$lang['en_US']['GalleryImage']['NOIMAGE'] = '(No Image)';
+
+$lang['en_US']['GalleryPage']['CAPTION'] = 'Caption';
+$lang['en_US']['GalleryPage']['THUMBNAIL'] = 'Thumbnail';
+$lang['en_US']['GalleryPage']['TITLE'] = 'Gallery Image';
+$lang['en_US']['GalleryPage']['HEADING'] = 'Gallery';
